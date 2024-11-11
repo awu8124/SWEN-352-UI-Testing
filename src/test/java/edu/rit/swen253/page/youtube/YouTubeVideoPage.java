e2047f9bccd7247143f4eee13830a6590afea6db
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2024-11-10 22:59:31 -0500

    Add YouTubeVideoPage class for YouTube video page testing

---
+++ src/test/java/edu/rit/swen253/page/youtube/YouTubeVideoPage.java
@@ -0,0 +1,19 @@
+package edu.rit.swen253.page.youtube;
+
+import edu.rit.swen253.page.AbstractPage;
+import edu.rit.swen253.utils.SeleniumUtils;
+import org.openqa.selenium.support.PageFactory;
+
+public class YouTubeVideoPage extends AbstractPage {
+
+    public YouTubeVideoPage() {
+        PageFactory.initElements(SeleniumUtils.getDriver(), this);
+    }
+
+    /**
+     * Method to get the page title.
+     */
+    public String getPageTitle() {
+        return SeleniumUtils.getDriver().getTitle();
+    }
+}
