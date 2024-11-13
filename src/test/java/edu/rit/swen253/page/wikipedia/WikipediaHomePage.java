012fa78da91311afa7113aa43deec6a244cd20de
Alana Wu <aw4857@g.rit.edu>
2024-11-13 00:08:35 -0500

    wikipedia testing & page files

--- src/test/java/edu/rit/swen253/page/wikipedia/WikipediaHomePage.java
+++ src/test/java/edu/rit/swen253/page/wikipedia/WikipediaHomePage.java
@@ -1,63 +1,30 @@
-package test.java.edu.rit.swen253.page.wikipedia;
+package edu.rit.swen253.page.wikipedia;
 
 import edu.rit.swen253.page.AbstractPage;
-import edu.rit.swen253.utils.DomElement;
-import edu.rit.swen253.utils.HtmlUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-
-import java.util.List;
-import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.fail;
+import edu.rit.swen253.page.PageFactory;
+import edu.rit.swen253.utils.SeleniumUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class WikipediaHomePage extends AbstractPage {
-    private static final Logger logger = Logger.getLogger(WikipediaHomePage.class.getName());
-  
-    /**
-     * A finder to extract the main body content.
-     * WARN: this is fragile code
-     */
-    private static final By MAIN_CONTENT_FINDER = By.cssSelector("main > div > div.field--name-field-content");
-    
-    private DomElement mainContentPanel;
-  
+
+    @FindBy(id = "searchInput")
+    private WebElement searchInput;
+
     public WikipediaHomePage() {
-      super();
-      // validate basic page structure
-      try {
-        mainContentPanel = findOnPage(MAIN_CONTENT_FINDER);
-      } catch (TimeoutException e) {
-        fail("Main content panel not found");
-      }
+        SeleniumUtils.getDriver().get("https://www.wikipedia.org/");
+        // Wait for video page to load
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Wikipedia"));
     }
-  
-    // public List<RitAreaOfStudyLink> getStudyLinks() {
-    //   // the Ratings panel is in the fifth <div> of the main content
-    //   // WARN: this is fragile code
-    //   return mainContentPanel.findChildBy(By.xpath("div[7]"))
-    //     // the statistics columns are organized in an unordered list embedded in a layout div
-    //     .findChildBy(By.cssSelector("div.row ul"))
-    //     // extract each statistics columns
-    //     .findChildrenBy(By.cssSelector("li > div.card"))
-    //     .stream()
-    //     // build a Rating Info view object for each of these divs
-    //     .map(RitAreaOfStudyLink::new)
-    //     .toList();
-    // }
-  
-    // public List<RatingInfoView> getRatingViews() {
-    //   // the Ratings panel is in the fifth <div> of the main content
-    //   // WARN: this is fragile code
-    //   return mainContentPanel.findChildBy(By.xpath("div[5]"))
-    //     // the statistics columns are organized in a layout div
-    //     .findChildBy(By.cssSelector("div.row"))
-    //     // extract each statistics columns
-    //     .findChildrenBy(By.cssSelector("div.col-statistic"))
-    //     .stream()
-    //     // build a Rating Info view object for each of these divs
-    //     .map(RatingInfoView::new)
-    //     .toList();
-    // }
+
+    public void enterSearchPhrase(String phrase){
+        searchInput.sendKeys(phrase);
+        searchInput.sendKeys(Keys.RETURN);    
+    }
+
   }
   
---
+++ src/test/java/edu/rit/swen253/page/wikipedia/WikipediaResultsPage.java
@@ -0,0 +1,41 @@
+package edu.rit.swen253.page.wikipedia;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import edu.rit.swen253.page.AbstractPage;
+import edu.rit.swen253.utils.SeleniumUtils;
+import java.util.List;
+
+public class WikipediaResultsPage extends AbstractPage{
+    
+    @FindBy(css = "div.mw-search-result-heading > a")
+    private List<WebElement> searchResults;
+
+    public WikipediaResultsPage() {
+        PageFactory.initElements(SeleniumUtils.getDriver(), this);
+    }
+
+    public void clickFirstResult() {
+        if (!searchResults.isEmpty()) {
+            searchResults.get(0).click();
+        } else {
+            System.out.println("No search results available");
+        }
+    }
+
+    public void  logResults(){
+        for (WebElement result : searchResults) {
+            try {
+                String title = result.getText();
+                String url = result.getAttribute("href");
+                System.out.println("Title: " + title + ", URL: " + url);
+            } catch (RuntimeException e) {
+                System.out.println("Error finding Wikipedia title or URL for a result: " + e.getMessage());
+            }
+        }
+    }
+
+    
+}
